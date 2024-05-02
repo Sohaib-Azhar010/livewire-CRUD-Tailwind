719b02e60fc1946b39f2c9eeef4d7d40ee5c891c
Sohaib-Azhar010 <sohaibazhar04@gmail.com>
2024-05-02 19:43:57 +0500

    first commit

---
+++ app/Http/Controllers/SubContractorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SubContractor;
+use Illuminate\Http\Request;
+
+class SubContractorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SubContractor $subContractor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SubContractor $subContractor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SubContractor $subContractor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SubContractor $subContractor)
+    {
+        //
+    }
+}
